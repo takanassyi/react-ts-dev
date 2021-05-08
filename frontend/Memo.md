0003d143d9806e10596101faa4b982d4875f2ac1
takanassyi <takanassyi@gmail.com>
2021-05-08 18:46:52 +0900

    modified

---
+++ frontend/Memo.md
@@ -0,0 +1,4 @@
+# 参考リンク
+
+- https://qiita.com/kojiro_ueda/items/a490f5dfc77f5f89ecbd
+- https://qiita.com/ysd_marrrr/items/f963210760627ab1a9be
---
+++ frontend/pages/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+const Index: NextPage = (props) => {
+  return <h1>Hello Next.js!!</h1>;
+};
+
+export default Index;
